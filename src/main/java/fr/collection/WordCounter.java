7f1e51bdb331ce8c056965a2600b53f0a4d9acbc
Ludovic Esperce <ludovic.esperce@afpa.fr>
2024-07-08 14:54:38 +0200

    Ajout de commentaire sur l'exemple de décompte des mots avec un HashMap

--- src/main/java/fr/collection/WordCounter.java
+++ src/main/java/fr/collection/WordCounter.java
@@ -5,20 +5,28 @@ import java.util.Map;
 
 
 /**
- * Exemple d'utilisation d'objet de la classe "HashMap".
+ * Exemple d'utilisation d'objet de la classe "HashMap"
  * 
  */
 public class WordCounter {
 
     public static Map<String, Integer> countWordFrequency(String sentence) {
+        // création du tableau associatif résultat
         Map<String, Integer> wordCount = new HashMap<>();
+
+        // construction d'un tableau 1D de chaînes de caractères
+        // en divisant la chaîne avec les espaces
         String[] words = sentence.split("\\s+");
 
+        // on passe en revue tous les mots
         for (String word : words) {
+            // passage en minuscules
             word = word.toLowerCase();
+            // ajout des duo "clé-valeur" au tableau associatif
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
-
+        
+        // on retourne le tableau associatif
         return wordCount;
     }
 }
